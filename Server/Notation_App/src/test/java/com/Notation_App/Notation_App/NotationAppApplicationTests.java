8b196eba0466e04a6f8acef471972811d9aacf82
Gimhansathsara843 <160720393+Gimhansathsara843@users.noreply.github.com>
2024-07-08 22:55:55 +0530

    create Spring Boot File

---
+++ Server/Notation_App/src/test/java/com/Notation_App/Notation_App/NotationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Notation_App.Notation_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
