46479c8220d65e9058873f6190f2c54822455344
Gimhansathsara843 <160720393+Gimhansathsara843@users.noreply.github.com>
2024-09-11 14:09:29 +0530

    add new back end

---
+++ server/notationApp/src/main/java/notationApp/notationApp/NotationAppApplication.java
@@ -0,0 +1,13 @@
+package notationApp.notationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotationAppApplication.class, args);
+	}
+
+}
---
+++ server/notationApp/src/test/java/notationApp/notationApp/NotationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package notationApp.notationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
